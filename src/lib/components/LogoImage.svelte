ca5f7a5d7f9c6d7fd373885171f6518143888e9c
Pablo Cortés Rodríguez <cortesrodriguezpablo3@gmail.com>
2023-12-13 16:19:39 +0100

    First version

---
+++ src/lib/components/LogoImage.svelte
@@ -0,0 +1,54 @@
+<script>
+    export let title;
+</script>
+
+<a class="logo-container site-title" href="/">
+    <img class="logo-image" src="/logo.png" alt="">
+    <h1 class="logo logo-header">{title}</h1>
+</a>
+
+
+<style>
+    .logo-container {
+        text-align: center;
+        display: grid;
+        grid-template-columns: auto 1fr;
+        max-width: fit-content;
+        place-items: center;
+    }
+    /* .logo {
+        font-family: 'comfortaa';
+        font-weight: 900;
+        color: #341B4D;
+        width: max-content;
+    }
+
+    .logo-container {
+        display: grid;
+        grid-template-columns: auto auto;
+        justify-self: left;
+        align-items: center;
+        height: fit-content;
+        border-radius: 18px;
+        background-color: white;
+        padding: 4px 10px;
+        width: fit-content;
+    }
+ */
+    .logo-container h1 {
+        max-width: fit-content;
+        font-size: 20px;
+        margin: 0;
+        padding: 0;
+        outline-bottom: none !important;
+    }
+
+    .logo-image {
+        width: 44px;
+        margin: 10px 10px 10px 0;
+    }
+
+</style>
+
+
+
---
+++ src/lib/config.js
@@ -0,0 +1,26 @@
+/**
+ * All of these values are used throughout the site – for example, 
+ * in the <meta> tags, in the footer, and in the RSS feed.
+ * 
+ * PLEASE BE SURE TO UPDATE THEM ALL! Thank you!
+ **/ 
+
+export const siteTitle = 'ClientsHunter'
+export const siteDescription = 'Start to master the art of sales'
+export const siteURL = 'clientsHunter.co'
+export const siteLink = 'https://github.com/josh-collinsworth/sveltekit-blog-starter'
+export const siteAuthor = '- Pablo Cortés'
+
+// Controls how many posts are shown per page on the main blog index pages
+export const postsPerPage = 10
+
+// Edit this to alter the main nav menu. (Also used by the footer and mobile nav.)
+export const navItems = [
+	{
+		title: 'Blog',
+		route: '/blog'
+	}, {
+		title: 'Contact us',
+		route: '/contact' 
+	},
+]
