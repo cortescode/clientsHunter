01ca9f0138d3a30066f22e08cd4c3ae698c17bbc
Pablo Cortés Rodríguez <cortesrodriguezpablo3@gmail.com>
2023-12-15 18:32:16 +0100

    Another Trial

--- src/routes/blog/category/[category]/page/[page]/+page.server.js
+++ src/routes/blog/category/[category]/page/[page]/+page.server.js
@@ -1,6 +1,7 @@
 import { redirect } from '@sveltejs/kit'
 import { postsPerPage } from '$lib/config'
 import fetchPosts from '$lib/assets/js/fetchPosts'
+import { base } from "$app/paths"
 
 export const load = async ({ url, params, fetch }) => {
 	const page = parseInt(params.page) || 1
@@ -8,7 +9,7 @@ export const load = async ({ url, params, fetch }) => {
 
 	// Prevents duplication of page 1 as the index page
 	if (page <= 1) {
-		throw redirect(301, `/blog/category/${category}`);
+		throw redirect(301, `${ base }/blog/category/${category}`);
 	}
 	
 	let offset = (page * postsPerPage) - postsPerPage
